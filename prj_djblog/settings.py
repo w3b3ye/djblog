4221e184a9637ed496fa7067493e5c25535f6228
w3b3ye <w3b3ye@gmail.com>
2021-02-06 17:55:10 +0000

    Added 'account' app.

--- prj_djblog/settings.py
+++ prj_djblog/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #My Apps
+    'account',
+
+    #Default Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

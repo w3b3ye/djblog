1736872f2d1368c2593f55f798b957f837fb96b7
w3b3ye <w3b3ye@gmail.com>
2021-02-14 19:56:17 +0000

    Completed Account update form.

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 
-from account.forms import RegistrationForm, AccountAuthenticationForm
+from account.forms import RegistrationForm, AccountAuthenticationForm, AccountUpdateForm
 
 def registration_view(request):
 	context = {}
@@ -49,3 +49,24 @@ def login_view(request):
 	
 	context['login_form'] = form
 	return render(request, 'account/login.html', context)
+
+def account_view(request):
+	if not request.user.is_authenticated:
+		return redirect('login')
+
+	context = {}
+
+	if request.POST:
+		form = AccountUpdateForm(request.POST, instance=request.user)
+		if form.is_valid():
+			form.save()
+	else:
+		form = AccountUpdateForm(
+			initial= {
+				'email': request.user.email,
+				'username': request.user.username,
+			}
+
+		)
+	context['account_form'] = form
+	return render(request, 'account/account.html', context)
--- prj_djblog/urls.py
+++ prj_djblog/urls.py
@@ -23,7 +23,9 @@ from personal.views import (
 from account.views import (
     registration_view,
     logout_view,
-    login_view
+    login_view,
+    account_view,
+
 )
 
 urlpatterns = [
@@ -32,4 +34,5 @@ urlpatterns = [
     path('register/', registration_view, name='register'),
     path('logout/', logout_view, name='logout'),
     path('login/', login_view, name='login'),
+     path('account/', account_view, name='account'),
 ]

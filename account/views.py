a513df24d4e4053a67e5f834d5466bfd7483c99b
w3b3ye <w3b3ye@gmail.com>
2021-02-14 19:02:33 +0000

    Created form for login.

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 
-from account.forms import RegistrationForm
+from account.forms import RegistrationForm, AccountAuthenticationForm
 
 def registration_view(request):
 	context = {}
@@ -25,3 +25,27 @@ def registration_view(request):
 def logout_view(request):
 	logout(request)
 	return redirect('home')
+
+def login_view(request):
+	context = {}
+
+	user = request.user
+	if user.is_authenticated:
+		return redirect('home')
+	
+	if request.POST:
+		form = AccountAuthenticationForm(request.POST)
+		if form.is_valid():
+			email = request.POST['email']
+			password = request.POST['password']
+			user = authenticate(email=email, password=password)
+
+			if user:
+				login(request, user)
+				return redirect('home')
+
+	else:
+		form = AccountAuthenticationForm()
+	
+	context['login_form'] = form
+	return render(request, 'account/login.html', context)
